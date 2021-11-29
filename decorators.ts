edfcb411a20ae83a1479aa9c295d9f6629c67af0
Eduardo Vilke <eduardo@twoweb.com.br>
2021-11-29 17:35:08 -0300

    decorators

---
+++ decorators.ts
@@ -0,0 +1,92 @@
+
+// // Factory
+// function Logger(prefix: string){
+//   return (target: any) => {
+//     console.log(`${prefix} - ${target}`);
+//   };
+// }
+
+
+// @Logger('awesome')
+// class Foo {}
+
+// class decorator
+// function setAPIVersion(apiVersion:string) {
+//   return (constructor: any) => {
+//     return class extends constructor {
+//       version = apiVersion
+//     }
+//   }
+// }
+
+// // decorator - anotar versao da API
+// @setAPIVersion("1.0.0")
+// class API {}
+// console.log(new API());
+
+// Property decorator
+// function minLength(length: number) {
+//   return (target: any, key: string) => {
+//     let val = target[key]
+
+//     const getter = () => val;
+
+//     const setter = (value: string) => {
+//       if(value.length < length){
+//         console.log(`Error: você não pode criar ${key} com tamanho menor que ${length}`);
+//       }else {
+//         val = value
+//       }
+//     }
+
+//     Object.defineProperty(target, key, {
+//       get: getter,
+//       set: setter,
+//     })
+//   }
+// }
+
+// class Movie {
+//   @minLength(50)
+//   title: string;
+
+//   constructor(t: string){
+//     this.title = t;
+//   }
+// }
+
+// const movie = new Movie("Interestelar")
+// console.log(movie.title);
+
+// Method decorator
+
+function delay(ms: number){
+  return (target: any, key: string, descriptor: PropertyDescriptor) => {
+   const originalMethod = descriptor.value;
+
+   descriptor.value = function(...args: any) {
+     console.log(`Esperando ${ms}...`);
+     setTimeout(() => {
+       originalMethod.apply(this, args)
+     }, ms);
+     return descriptor;
+   }
+  }
+}
+
+class Greeter {
+  greeting: string;
+
+  constructor(g:string){
+    this.greeting = g
+  }
+
+  // esperar um tempo e aí vai rodar o método (ms)
+  @delay(5000)
+  greet(){
+    console.log(`Hello ${this.greeting}`);
+  }
+}
+
+const pessoinha = new Greeter("Pessoinha");
+pessoinha.greet();
